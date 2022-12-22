8478610858a13bbbabe0b734807178b8e3f4a6f7
meli-greyna <guillermo.reyna@mercadolibre.com>
2022-12-22 12:16:05 -0300

    trying things out

--- hello-meli.go
+++ hello-meli.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("hello meli")
+	var pressure, _ = strconv.ParseFloat(os.Args[1], 64)
+	var humidity, _ = strconv.ParseFloat(os.Args[2], 64)
+	var temperature, _ = strconv.ParseFloat(os.Args[3], 64)
+
+	fmt.Println(pressure, humidity, temperature)
 }
